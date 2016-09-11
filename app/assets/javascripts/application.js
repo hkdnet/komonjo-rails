8dfb9bc3b2733b0b56bd90edf40a6a6276edf370
hkdnet <hkdnet@users.noreply.github.com>
2016-09-11 18:39:37 +0900

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require_tree .
+
+var hoge = require('hoge');
+console.log(hoge(1));
