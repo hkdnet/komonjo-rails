b20033bff417282b0391584b8faff0afcbcaea18
hkdnet <hkdnet@users.noreply.github.com>
2016-10-12 07:09:22 +0900

    Remove unused filters

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,22 +1,11 @@
 module Api
   class MessagesController < ApiController
     def index
-      @messages = fetch_messages.map { |m| converter.convert(m) }
-    end
-
-    def converter
-      @converter ||= Komonjo::MessageConverter.new(filters)
+      @messages = fetch_messages
     end
 
     def fetch_messages
       client.messages(params[:channel_name])
-    end
-
-    def filters
-      [
-        Komonjo::MessageFilters::ReplyFilter,
-        Komonjo::MessageFilters::UrlFilter
-      ]
     end
   end
 end
